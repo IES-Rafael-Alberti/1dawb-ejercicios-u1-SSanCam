3d6ac6bbf838e1f3cb5f584da90b8564a292f32b
Sara S Camilleri <ssancam391@g.educaand.es>
2023-10-31 23:07:09 +0100

    prueba1

--- src/prueba1.py
+++ src/prueba1.py
@@ -3,7 +3,7 @@ def mayor_igual(numero1, numero2):
     if (numero1 > numero2):
         return numero1
     elif(numero1 < numero2):
-        return numero2
+        return numero1
         
     else:
         return 0
